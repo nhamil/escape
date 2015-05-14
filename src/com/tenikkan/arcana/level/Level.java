56e4f8e2deb01ac81258d2f285dcc5f2679d242d
Thinic <hamilton.nick.1@gmail.com>
2015-05-14 09:29:39 -0500

    work on level

--- src/com/tenikkan/arcana/level/Level.java
+++ src/com/tenikkan/arcana/level/Level.java
@@ -2,5 +2,41 @@ package com.tenikkan.arcana.level;
 
 public class Level
 {
+    private int tiles[];
+    private int width, height;
     
+    public Level(int width, int height) 
+    {
+        this.width = width;
+        this.height = height;
+        
+        initTiles();
+    }
+    
+    public void setTileID(int x, int y, int id) 
+    {
+        if(!inBounds(x, y)) return;
+        tiles[x + y*width] = id;
+    }
+    
+    public int getTileID(int x, int y) 
+    {
+        if(!inBounds(x, y)) return -1;
+        return tiles[x + y*width];
+    }
+    
+    public Tile getTile(int x, int y) 
+    {
+        return TileManager.get(getTileID(x, y));
+    }
+    
+    public boolean inBounds(int x, int y) 
+    {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    }
+    
+    private void initTiles() 
+    {
+        tiles = new int[width*height];
+    }
 }
--- src/com/tenikkan/arcana/level/Tile.java
+++ src/com/tenikkan/arcana/level/Tile.java
@@ -5,17 +5,19 @@ public abstract class Tile
     private int id;
     private String name;
     private boolean solid;
+    private int color;
     
     public Tile(String name, int id, boolean solid, int color) 
     {
         this.name = name;
         this.id = id;
+        this.solid = solid;
+        this.color = color;
         
         TileManager.add(this);
     }
     
-    public abstract void update(Level level, int x, int y, int data);
-    public abstract void render();
+    public int getColorCode() { return color; }
     
     public boolean isSolid() { return solid; }
     
